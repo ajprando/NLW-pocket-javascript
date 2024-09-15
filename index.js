5de3f7f3c38f559c0c011efcd71fb111af2ded63
Anna <annaprando04@gmail.com>
2024-09-14 23:25:14 -0300

    cadastrar meta

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
-const { select } = require('@inquirer/prompts')
+const { select, input } = require('@inquirer/prompts')
+
+let meta = {
+    value: 'Tomar 3L de água por dia',
+    checked: false,
+}
+let metas = [ meta ]
+
+const cadastraMeta = async () => {
+    const meta = await input({message: "Digite a meta:"})
+
+    if(meta.length == 0) {
+        console.log("A meta não pode ser vazia.")
+        return
+    }
+
+    metas.push(
+        { value: meta, checked: false }
+    )
+    
+}
 
 const start = async () => {
 
@@ -25,8 +45,8 @@ const start = async () => {
 
         switch(opcao) {
             case "cadastrar":
-                console.log("vamos cadastrar")
-                break
+                await cadastraMeta()
+                console.log(metas)
             case "listar": 
                 console.log("vamos listar")
                 break
